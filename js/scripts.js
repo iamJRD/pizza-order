b9a7f084850b61410042034a4494ea384268776f
Jared Beckler <jared.beckler@gmail.com>
2016-01-22 08:22:53 -0800

    rename folder from 'spec' to 'specs' and add initial spec test for numbers % 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
+var pingPonging = function(number){
+  if(number % 3 === 0){
+    return "ping";
+  }
 
+}
---
+++ specs/specs.js
@@ -0,0 +1,6 @@
+describe("pingPonging", function(){
+  it("will return 'ping' if the number is divisable by only 3", function(){
+    expect(pingPonging(6)).to.equal("ping");
+  });
+  
+});
