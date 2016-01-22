9a7887d7ab071a59d73542337c0a2448b33e0833
Jared Beckler <jared.beckler@gmail.com>
2016-01-22 11:50:25 -0800

    add base styling to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var pingPonging = function(number){
   if ((number % 3 === 0) && (number % 5 === 0)){
-    return "PingPong";
+    return "Ping-Pong";
   } else if (number % 5 === 0){
     return "Pong";
   } else if (number % 3 === 0){
@@ -21,7 +21,7 @@ $(document).ready(function(){
       event.preventDefault();
     }
     else {
-      $("#numbersList").text("")
+      $("#numbersList").empty()
       for(i = 1; i <= userInput; i++){
       $("#numbersList").append("<li>" + pingPonging(i) + "</li>");
       }
--- specs/specs.js
+++ specs/specs.js
@@ -7,8 +7,8 @@ describe("pingPonging", function(){
     expect(pingPonging(20)).to.equal("Pong");
   });
 
-  it("will return 'PingPong' if the number is divisable by 3 and 5", function(){
-    expect(pingPonging(45)).to.equal("PingPong");
+  it("will return 'Ping-Pong' if the number is divisable by 3 and 5", function(){
+    expect(pingPonging(45)).to.equal("Ping-Pong");
   });
 
   it("will return the number if it is not divisable by 3 or 5", function(){
