32c904328f0f25f79dae3a2b8e90746b027f35eb
Jared Beckler <jared.beckler@gmail.com>
2016-01-22 08:38:01 -0800

    add spec test and functionality for numbers % 3 and 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 var pingPonging = function(number){
-  if (number % 3 === 0){
-    return "Ping";
-  }else if (number % 5 === 0){
+  if ((number % 3 === 0) && (number % 5 === 0)){
+    return "PingPong";
+  } else if (number % 5 === 0){
     return "Pong";
+  } else if (number % 3 === 0){
+    return "Ping";
   }
 
 }
--- specs/specs.js
+++ specs/specs.js
@@ -1,10 +1,14 @@
 describe("pingPonging", function(){
-  it("will return 'ping' if the number is divisable by only 3", function(){
+  it("will return 'Ping' if the number is divisable by only 3", function(){
     expect(pingPonging(9)).to.equal("Ping");
   });
 
-  it("will return 'pong' if the number is divisable by only 5", function(){
+  it("will return 'Pong' if the number is divisable by only 5", function(){
     expect(pingPonging(20)).to.equal("Pong");
+  });
+
+  it("will return 'PingPong' if the number is divisable by 3 and 5", function(){
+    expect(pingPonging(45)).to.equal("PingPong");
   });
 
 });
